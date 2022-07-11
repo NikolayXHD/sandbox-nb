25eb692fe4db7711061b9401890ab94760d65d64
NikolayXHD <hidalgo1984@list.ru>
2022-07-12 02:07:31 +0300

    reg_2d: plot dln_exp_no_vol_log_72d vs dln_exp_no_vol_log_3d

--- notebooks/regression/reg_2d.py
+++ notebooks/regression/reg_2d.py
@@ -256,6 +256,29 @@ plot_facet_2d(
     indicator_1_field='dln_exp_no_vol_log_3d',
     min_x0=-0.80,
     max_x0=+0.80,
+    indicator_2_field='dln_exp_no_vol_log_72d',
+    min_x1=-0.80,
+    max_x1=+0.80,
+    alpha_min=0.1,
+    v_min_color=-1.5,
+    v_max_color=+1.5,
+    levels=(
+        *np.linspace(-6.0, -2.8, num=5),
+        *np.linspace(-2.0, -0.4, num=5),
+        *(-0.2, -0.1, 0, +0.1, +0.2),
+        *np.linspace(+0.4, +2.0, num=5),
+        *np.linspace(+2.8, +6.0, num=5),
+    ),
+    regression_bins=(200, 200, 1),
+    radius=0.04,
+    log_alpha_scale=True,
+)
+
+# %%
+plot_facet_2d(
+    indicator_1_field='dln_exp_no_vol_log_3d',
+    min_x0=-0.80,
+    max_x0=+0.80,
     indicator_2_field='dln_exp_no_vol_log_24d',
     min_x1=-0.80,
     max_x1=+0.80,
